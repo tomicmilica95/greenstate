f1140ce399168f19916f4cca75a184e84eab1a1d
tomicmilicaa <115076438+tomicmilicaa@users.noreply.github.com>
2024-03-04 10:46:03 +0100

    remove index.css

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
 import i18next from 'i18next';
 
 import { App } from './App';
-import './index.css';
 import './i18n';
 import { Provider } from 'react-redux';
 import store from './redux/store';
